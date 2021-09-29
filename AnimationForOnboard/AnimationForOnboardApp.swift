880e0115d924641c308e367ff753e3976289e45f
ihValery <ihValery@gmail.com>
2021-09-29 04:14:28 +0300

    Initial Commit

---
+++ AnimationForOnboard/AnimationForOnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationForOnboardApp.swift
+//  AnimationForOnboard
+//
+//  Created by Валерий Игнатьев on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationForOnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
